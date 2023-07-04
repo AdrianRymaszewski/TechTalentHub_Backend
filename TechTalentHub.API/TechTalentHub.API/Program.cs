c0fb2bc24feb2ce36ed39ad9b0a29e00ddaeb77f
AdrianRymaszewski <arymasz87@gmail.com>
2023-07-04 19:26:37 +0200

    Config swagger for azure

--- TechTalentHub.API/TechTalentHub.API/Program.cs
+++ TechTalentHub.API/TechTalentHub.API/Program.cs
@@ -110,10 +110,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
+}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
